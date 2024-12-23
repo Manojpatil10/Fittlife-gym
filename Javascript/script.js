8918cead3eb6e239f2b83b73fb51b8adb4445470
Manojpatil10 <manojrpatil1000@gmail.com>
2024-12-23 23:03:30 +0530

    code updated

--- Javascript/script.js
+++ Javascript/script.js
@@ -4,10 +4,8 @@ $(document).ready(function(){
 
   $(".stories-slider").owlCarousel({
     loop:true,
-    // autoplay:true,
+    autoplay:true,
     margin:50,
-    // dots:false,
-    // nav:true,
     responsive:{
       0:{
         items:1
@@ -17,5 +15,30 @@ $(document).ready(function(){
       }
     }
   });
+
+  $(".trainer-carousel").owlCarousel({
+    loop:true,
+    autoplay:true,
+    margin:20,
+    responsive:{
+      0:{
+        items:1
+      },
+      768:{
+        items:2
+      },
+      1200:{
+        items:3
+      },
+      1400:{
+        items:4
+      }
+    }
+  });
+
+  $('.ham-menu').hide();
+  $('.menu-button').click(function(){
+    $('.ham-menu').fadeToggle(1);
+  })
   
 });
